01b04d65acb3d378f4c2236cdd662924a09ea367
Thiago <tlkgaribaldi@gmail.com>
2021-02-16 07:49:16 -0300

    inicial

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const e = require('express');
+const express = require('express');
+const routes =  require('./routes');
+
+require('./database');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
