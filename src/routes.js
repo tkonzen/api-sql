525a76cc35f72d091754e4b64f44eee323a7ad36
Thiago <tlkgaribaldi@gmail.com>
2021-02-16 08:13:43 -0300

    tres

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,5 @@ routes.post('/users', UserController.store);
 routes.get('/users/:user_id/addresses', AddressController.index);
 routes.post('/users/:user_id/addresses', AddressController.store);
 
-//
-
 
 module.exports = routes;
